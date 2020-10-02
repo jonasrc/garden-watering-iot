8407edbd19513685df8f3700a9cb086a294a1ee3
Jonas Rocha Castanheira <jonasrcastanheira@gmail.com>
2020-10-02 00:01:13 -0300

    - Adjusting AJAX calls to use jQuery wrapper methods; - Adjusting AJAX calls to perform accordingly to ESP8266's interface; - Reconstructing valve control logic inside ESP8266; - Removing old control logic from JS; - Changing ESP8266 server handling module to its asynchronous version; - Modularizing ESP8266 internal methods; - JS code cleaning.

--- garden_watering_iot.ino
+++ garden_watering_iot.ino
@@ -1,10 +1,15 @@
 #include <FS.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+//#include <ESP8266WebServer.h>
+#include "ESPAsyncWebServer.h"
 
 //On board LED Connected to GPIO2
 #define LED 2  
+#define HUMIDITY_SENSOR A0  
+#define AUTOMATIC_MODE "auto"
+#define MANUAL_MODE "manual"
+#define CONTROL_INTERVAL 1000
 
 int ledStatus = LOW;
 
@@ -13,26 +18,105 @@ const char* password = "25408195";
 
 String webPage = "";
 
-ESP8266WebServer server(80);
+//Automatic control function interval variables
+unsigned long lastAutoControlTime;
 
-void handleRoot() {
-  server.send(200, "text/html", webPage);
+//Automatic control targets
+String controlMode = MANUAL_MODE;
+int targetHumidity = 0;
+
+AsyncWebServer server(80);
+
+void handleRoot(AsyncWebServerRequest *request) {
+  request->send(200, "text/html", webPage);
 }
 
-void handleToggleLight() {
-  Serial.print("Controlling LED... Status is: ");
-  Serial.println(ledStatus);
-  ledStatus = 1 - ledStatus;
-  digitalWrite(LED, ledStatus);
-  server.send(200, "application/json", "");
-}
-
-void handleGetHumidity() {
-  int humidity = analogRead(A0);
-  humidity = map(humidity, 854, 1024, 0, 100);
+void handleGetHumidity(AsyncWebServerRequest *request) {
   char humidityBuffer[3];
-  itoa(humidity, humidityBuffer, 10);
-  server.send(200, "text/plain", humidityBuffer);
+  itoa(getHumidity(), humidityBuffer, 10);
+  request->send(200, "text/plain", humidityBuffer);
+}
+
+void handleControlValve(AsyncWebServerRequest *request) {
+  int paramsNumber = request->params();
+  if(paramsNumber > 3 || paramsNumber == 0){
+    //TODO Throw exception
+    Serial.println("ERROR - Invalid number of query parameters passed to /control-valve.");
+    return;
+  }
+  
+  AsyncWebParameter* valveMode;
+  AsyncWebParameter* valveState;
+  AsyncWebParameter* humidity;
+
+  for(int i = 0; i < paramsNumber; i++){
+    AsyncWebParameter* param = request->getParam(i);
+    
+    if(param->name() == "mode"){
+      valveMode = param;
+    }
+    else if(param->name() == "state"){
+      valveState = param;
+    }
+    else if(param->name() == "humidity"){
+      humidity = param;
+    }
+    else {
+      //TODO throw Exception
+      Serial.println("ERROR - Invalid query parameter passed to /control-valve.");
+      return;
+    }
+  }
+
+  if(valveMode->value() == MANUAL_MODE){ 
+    controlMode = MANUAL_MODE;
+    targetHumidity = 0;
+    toggleValve(valveState->value().toInt());
+    String stateString = valveState->value().toInt() ? "ON" : "OFF";
+    request->send(200, "text/plain", "Automatic control turned OFF. Valve turned " + stateString + " via manual mode.");
+  }
+  else if(valveMode->value() == AUTOMATIC_MODE){
+    controlMode = valveState->value().toInt() ? AUTOMATIC_MODE : MANUAL_MODE;
+    targetHumidity = humidity->value().toInt();
+    String stateString = valveState->value().toInt() ? "ON" : "OFF";
+    request->send(200, "text/plain", "Automatic control turned " + stateString + ".");
+  }
+  else {
+    Serial.println("ERROR - Valve operating mode not recognized.");
+  }
+}
+
+int getHumidity() {
+  //TODO Improve sensor range mapping (does not go to 0 apparently)
+  return map(analogRead(HUMIDITY_SENSOR), 1024, 0, 0, 100);
+}
+
+void automaticControl() {
+  
+    Serial.print("Executing automatic control at time: ");
+    Serial.println(millis());
+
+    int newValveState = LOW;
+    if((targetHumidity > realHumidity - HUMIDITY_DIFF_THRESHOLD) && (targetHumidity < realHumidity + HUMIDITY_DIFF_THRESHOLD)) {
+      newValveState = LOW;
+    }
+    else if(targetHumidity < realHumidity - HUMIDITY_DIFF_THRESHOLD) {
+      newValveState = LOW;
+    }
+    else if(targetHumidity > realHumidity + HUMIDITY_DIFF_THRESHOLD) {
+      newValveState = HIGH;
+    }
+    else {
+      newValveState = LOW;
+    }
+
+    toggleValve(newValveState);
+}
+
+void toggleValve(int state) {
+  Serial.print("Controlling mock valve... Status is: ");
+  Serial.println(state);
+  digitalWrite(LED, state);
 }
 
 void readFile(void) {
@@ -74,13 +158,17 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", handleRoot);
-  server.on("/toggle-light", handleToggleLight);
-  server.on("/humidity", handleGetHumidity);
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/humidity", HTTP_GET, handleGetHumidity);
+  server.on("/control-valve", HTTP_GET, handleControlValve);
 
   server.begin();
 }
 
 void loop() {
-  server.handleClient();
+  unsigned long currentTime = millis();
+  if(controlMode == AUTOMATIC_MODE && currentTime >= lastAutoControlTime + CONTROL_INTERVAL){
+    lastAutoControlTime = currentTime;
+    automaticControl();
+  }
 }
